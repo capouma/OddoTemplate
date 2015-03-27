dbb798b73dccf00e342c94375d09878ba61f486f
Carlos Pousa <cpousamarcote@danielcastelao.org>
2015-03-27 13:57:03 +0100

    Creacion de dummy_theme, modulo y academy

---
+++ usr/lib/python2.7/dist-packages/openerp/addons/openacademy/models.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+class MinimalModel(models.Model):
+    _name = 'test.model'
+    
+class LessMinimalModel(models.Model):
+    _name = 'test.model2'
+
+    name = fields.Char(required=True)
+    
+class Course(models.Model):
+    _name = 'openacademy.course'
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
