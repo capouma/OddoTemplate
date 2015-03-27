7665ca669e5cc40a2eed8e0424d8c1b5958944db
Carlos Pousa <cpousamarcote@danielcastelao.org>
2015-03-27 14:03:15 +0100

    Creacion de dummy_theme, modulo y academy

---
+++ usr/lib/python2.7/dist-packages/openerp/addons/dummy_theme/__openerp__.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python
+
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+{
+    # Theme information
+    'name': "Dummy Theme",
+    'description': """
+    """,
+    'category': 'Theme',
+    'version': '3.1',
+    'depends': ['website'],
+
+    # templates, pages, and snippets
+    'data': [
+        'views/pages.xml',
+        #'views/options.xml',
+        'views/snippets.xml',
+    ],
+
+    # Your information
+    'author': "CPousa",
+    'website': "",
+}
